bbb7ed99c08a55abb2276d289166e0847c10c6aa
Daniel Anechitoaie <daniel.anechitoaie@gmail.com>
2026-05-13 23:58:28 +0300

    feat: add ThemeProvider component for dark/light theme support

--- shhhare_app/src/main.tsx
+++ shhhare_app/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { CryptoGuard } from "@/guards/crypto";
+import { ThemeProvider } from "./components/ui/theme-provider";
 import { routeTree } from "./routeTree.gen";
 import "./main.css";
 
@@ -14,8 +15,10 @@ declare module "@tanstack/react-router" {
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <CryptoGuard>
-            <RouterProvider router={router} />
-        </CryptoGuard>
+        <ThemeProvider>
+            <CryptoGuard>
+                <RouterProvider router={router} />
+            </CryptoGuard>
+        </ThemeProvider>
     </StrictMode>,
 );
