2b5cf4049fac4df446c7bf67eb1ea6fd122e0188
Daniel Anechitoaie <daniel.anechitoaie@gmail.com>
2026-05-27 11:38:34 +0300

    refactor: remove ThemeProvider component and update main.tsx structure

--- shhhare_app/src/main.tsx
+++ shhhare_app/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { CryptoGuard } from "@/guards/crypto";
-import { ThemeProvider } from "./components/ui/theme-provider";
 import { routeTree } from "./routeTree.gen";
 import "@fontsource-variable/inter/wght.css";
 import "./main.css";
@@ -16,10 +15,8 @@ declare module "@tanstack/react-router" {
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <ThemeProvider>
-            <CryptoGuard>
-                <RouterProvider router={router} />
-            </CryptoGuard>
-        </ThemeProvider>
+        <CryptoGuard>
+            <RouterProvider router={router} />
+        </CryptoGuard>
     </StrictMode>,
 );
